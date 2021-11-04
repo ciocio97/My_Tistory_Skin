1d8ef54e6a1e39199c4ff293ba6e7aa413b933a9
ciocio97 <tmddusleo@icloud.com>
2021-11-04 23:05:30 +0900

    make function for gathering <h> tags offsetTop & add className bold to <h> selected <h> tag

--- images/script.js
+++ images/script.js
@@ -115,51 +115,38 @@ arrange_list.forEach((p) => {
   nav_ul.append(space);
 });
 
-console.log(nav_ul);
-
 const nav_ul_li = nav_ul.querySelectorAll('li');
-const nav_ul_li_top_location = [...nav_ul_li].map((el) => el.offsetTop);
-console.log(nav_ul_li);
-console.log(nav_ul_li_top_location);
-// for(let el of [...nav_ul_li]){
-//   console.log(el);
-//   console.log(el.offsetTop);
-// }
 
-// arrange_list.forEach((p) => {
-//   for(let item of p){
+// h1,h2,h3,h4 제목들 offsetTop 위치 모으는 함수
+function getElementsOffsetTop(){
+  let output = [];
+  for(let i=1; i<=4; i++){
+    const elements = document.querySelectorAll(`.entry-content h${i}`);
+    elements.forEach(el => output.push(el.offsetTop));
+  }
+  return output.sort((a, b) => a - b);
+}
 
-//     const content = item.querySelector('b').textContent;
-//     const ul = document.createElement('ul');
-//     const li = document.createElement('li');
+const range = getElementsOffsetTop();
 
-//     switch(item.tagName){
-//       case 'H1':
-//         break;
-//       case 'H2':
-//         break;
-//       case 'H3':
-//         break;
-//       case 'H4':
-//         break;
-//       default:
-
-//     }
-//   }
-// });
-
+// scrollEvent : 제목을 지나갈 때 side_nav에 강조 표시
 window.addEventListener('scroll', function(){
-  const list = nav_ul_li_top_location;
-  const height_body = document.body.offsetHeight;
-  const height = window.scrollY;
-  // for(let i=0; i<list.length; i++){
-  //   if(list[i] <= location && location <= list[i+1]){
-  //     console.log('IM here!!!!!')
-  //   }
-  // }
-  console.log("offsetTop : ", window.offsetTop);
-  console.log("offsetLeft : ", window.offsetLeft);
-  console.log("scrollHeight : ", window.scrollHeight);
+  let location = document.documentElement.scrollTop;
+  
+  for(let i=0; i<range.length; i++){
+    if(range[i]-180 <= location && location <= range[i+1]-180){
+      nav_ul_li[i].classList.add('bold');
+    }
+    else{
+      nav_ul_li[i].classList.remove('bold');
+    }
+  }
+  
 });
+
+// offsetParent & offsetLeft & offsetTop
+
+// 잘 쓰이는 프로퍼티는 아님. offset은 요소가 화면에서 차지하는 영역 전체 크기를 나타낸다.
+// offsetLeft : offsetParent를 기준으로 
 
 
